3a7167139b5792952295b5fc82a214a1e3964bd0
Vincent4364 <103389512+Vincent4364@users.noreply.github.com>
2025-05-31 09:02:54 +1200

    new files

--- medusa-config.ts
+++ medusa-config.ts
@@ -2,15 +2,16 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
-module.exports = defineConfig({
+export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || "http://localhost:3000",
+      adminCors: process.env.ADMIN_CORS || "http://localhost:7000",
+      authCors: process.env.AUTH_CORS || "http://localhost:7000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   }
+  // Do NOT add a "modules" section for stockLocation
 })
