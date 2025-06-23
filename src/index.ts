43a4463441bf3cb57b45584aa173005d15b003a0
Vincent4364 <103389512+Vincent4364@users.noreply.github.com>
2025-06-23 12:13:18 +1200

    Files

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import { bootstrap, runMigrations } from '@vendure/core';
 import { config } from './vendure-config';
 
-if (process.env.RUN_MIGRATIONS === 'true') {
-  runMigrations(config)
+runMigrations(config)
     .then(() => bootstrap(config))
     .catch(err => {
-      console.error('Migration failed:', err);
+        console.log(err);
     });
-} else {
-  bootstrap(config).catch(err => {
-    console.error('Bootstrap failed:', err);
-  });
-}
